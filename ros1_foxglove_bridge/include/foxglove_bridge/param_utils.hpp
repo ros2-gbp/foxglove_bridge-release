e53d9b34c69a73e9e9814af2150530e0850fb63c
Yadunund <yadunund@openrobotics.org>
2023-04-20 12:48:56 +0800

    Imported upstream version '0.5.3' of 'upstream'

---
+++ ros1_foxglove_bridge/include/foxglove_bridge/param_utils.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <regex>
+#include <string>
+#include <vector>
+
+#include <xmlrpcpp/XmlRpc.h>
+
+#include <foxglove_bridge/parameter.hpp>
+
+namespace foxglove_bridge {
+
+foxglove::Parameter fromRosParam(const std::string& name, const XmlRpc::XmlRpcValue& value);
+
+std::vector<std::regex> parseRegexPatterns(const std::vector<std::string>& strings);
+
+}  // namespace foxglove_bridge
