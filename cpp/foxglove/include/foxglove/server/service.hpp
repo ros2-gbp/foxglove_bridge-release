897977c5cbb2e7afca28adaaaefd8640a121fa5d
Chris Lalancette <chris.lalancette@foxglove.dev>
2026-08-25 17:14:07 +0000

    Trimmed the branch to only the ros/src/foxglove_msgs sub directory

--- cpp/foxglove/include/foxglove/server/service.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-#pragma message("This header is deprecated. Use <foxglove/service.hpp> instead.")
-#include <foxglove/service.hpp>
--- python/foxglove-sdk/python/docs/api/messages.rst
+++
@@ -1,2 +0,0 @@
-.. automodule:: foxglove.messages
-   :members:
